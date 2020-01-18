e6a68c0591f33a3caa31a329bf5896647369eed5
Andrew Bradley <cspotcode@gmail.com>
2020-01-18 06:47:31 -0500

    push

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+apt-get install git
+git clone https://github.com/cspotcode/typegoose
+cd typegoose
+git checkout repro-ts-node-898
 yarn
-yarn ts-node ./example.ts
+yarn mocha
