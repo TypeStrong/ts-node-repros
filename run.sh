562411ccc75fe76c83400a810b766a648919bc52
Andrew Bradley <cspotcode@gmail.com>
2020-01-31 21:14:30 -0500

    fix

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-n 12.13.0
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n && bash n 12.13.0
 yarn
 node -r ts-node/register/transpile-only  -r tsconfig-paths/register src/server.ts
