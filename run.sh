eb977dd459e5d88e0bd3bf9a48dabc636465026d
Andrew Bradley <cspotcode@gmail.com>
2020-01-31 21:25:28 -0500

    fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n && bash n 12.13.0
 yarn
 node -r ts-node/register/transpile-only  -r tsconfig-paths/register src/server.ts
-echo 'Done'
+echo "node exited with code: $?"
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,5 @@
 import './routers/mp2s';
+console.log('Done!');
+console.log();
+console.log();
+console.log();
