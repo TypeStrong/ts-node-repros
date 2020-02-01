c80987c9de20ac98f7722046bff24c5138c6c096
Andrew Bradley <cspotcode@gmail.com>
2020-01-31 21:23:22 -0500

    fix

--- src/routers/mp2s.ts
+++ src/routers/mp2s.ts
@@ -1,10 +1,5 @@
 import {MP2Controller, userCanAttachFiles} from '../controllers/MP2Controller';
 
-function ifUser(...args: any[]) {
-    return args;
-}
-const can = [];
-
 const test = [
-    ...userCanAttachFiles.map(can => ifUser(...can)),
+    ...userCanAttachFiles.map(v => v)
 ];
