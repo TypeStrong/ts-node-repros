24eb87b6ab4f298bb5b753c7fa3b0ab817c7c855
Andrew Bradley <cspotcode@gmail.com>
2020-02-09 17:14:28 -0500

    fix

--- npm-postinstall.ts
+++ npm-postinstall.ts
@@ -1,8 +1,9 @@
 //@ts-nocheck
 console.dir({
-    'module.id': module.id,
+    'process.cwd()': process.cwd(),
     __filename,
-    __dirname
+    __dirname,
+    module,
 });
 import * as x from './x';
 console.log('hello2', typeof x);
