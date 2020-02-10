466d7b360e2b4b52946244cd6fd709f93912427a
Andrew Bradley <cspotcode@gmail.com>
2020-02-10 18:42:25 -0500

    impl

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n && bash n 12
+node --version
+cd node_modules/my-project
+
 yarn
-yarn ts-node ./example.ts
+yarn tsc --project .
+ls -al
 echo "Process exited with code: $?"
 echo
 echo
