cfa846ab898ff62d5d942fbc1316050a74a3b483
Andrew Bradley <cspotcode@gmail.com>
2020-02-10 20:18:12 -0500

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 # Install a specific version of node
 n lts
@@ -7,7 +8,11 @@ n lts
 yarn
 
 # Run ts-node
-yarn ts-node ./example.ts
+time ./node_modules/.bin/tsc --project .
+time ./node_modules/.bin/ts-node ./run.ts
+time ./node_modules/.bin/ts-node --files ./run.ts
+time ./node_modules/.bin/ts-node --transpile-only ./run.ts
+time ./node_modules/.bin/ts-node --files --transpile-only ./run.ts
 
 echo "Process exited with code: $?"
 echo
