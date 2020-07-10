03bab64fe893025f71086c381478b6a91537869a
Andrew Bradley <cspotcode@gmail.com>
2020-07-10 14:59:30 -0400

    repro

--- example.ts
+++ example.ts
@@ -1,1 +1,8 @@
-console.log('Some .ts code reproducing a bug');
+{
+    const foobar = null;
+    console.log(foobar?.foo?.bar ?? "default value");
+}
+{
+    const foobar = {foo: {bar: 'foo.bar value'}};
+    console.log(foobar?.foo?.bar ?? "default value");
+}
