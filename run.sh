fdff59c682da5872ae034af3b24433668598ab6e
Andrew Bradley <cspotcode@gmail.com>
2020-10-25 18:21:29 -0400

    repro

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+git submodule update --init
+
 # Install a specific version of node
 n lts
 
 # Install package.json dependencies
 yarn
 
-# Run ts-node
-yarn ts-node ./example.ts
+cd node-ts-error
+npm install
+./node_modules/.bin/tsc
 
 echo "Process exited with code: $?"
 echo
