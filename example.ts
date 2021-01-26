4b1634ca795d10080f7a4db72e4eb973f6871aa7
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-27 00:30:02 +0800

    String indexing is incorrect with certain CJK Extension characters when run using ts-node

--- example.ts
+++ example.ts
@@ -1,1 +1,7 @@
-console.log('Some .ts code reproducing a bug');
+let z = "⿳⿻[:-1]𦫻口儿⿻⿸𰀪㇄口";
+let output = "";
+for (let char of z) {
+  output += char;
+  console.log(char.charCodeAt(0));
+  console.log(output);
+}
