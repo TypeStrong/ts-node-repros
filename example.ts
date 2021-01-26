62ac2bc6dc5c437300e94acab252ab2f3af0161f
Bryan Kok <bryan.wyern1@gmail.com>
2021-01-27 00:32:27 +0800

    Issue persists even with an explicit for loop

--- example.ts
+++ example.ts
@@ -5,3 +5,12 @@ for (let char of z) {
   console.log(char.charCodeAt(0));
   console.log(output);
 }
+
+console.log("with an explicit for loop:");
+output = "";
+for (let i = 0; i < z.length; i++) {
+  let char = z[i];
+  output += char;
+  console.log(char.charCodeAt(0));
+  console.log(output);
+}
