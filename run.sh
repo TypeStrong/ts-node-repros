49fb04ba34c838e18e49b9f72060d6a96768d3ee
Andrew Bradley <cspotcode@gmail.com>
2021-03-25 17:03:10 -0400

    fix

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ npm install -g npm-run-all
 # install jq
 mkdir bin
 curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > bin/jq
+chmod +x bin/jq
 export PATH="$PATH:$PWD/bin"
 # quick test to be sure it worked
 echo '{"foo": 1}' | jq .foo
@@ -19,31 +20,33 @@ git checkout main
 # git checkout jsjoeio/ts-node-repro
 yarn
 
+waittime=60
+
 # Run with old version
 yarn watch &
 job="$!"
-sleep 20
+sleep "$waittime"
 kill -9 "$job"
 
 # Run with 9.0.0 (explicit)
 yarn add -D ts-node@9.0.0
 yarn watch &
 job="$!"
-sleep 20
+sleep "$waittime"
 kill -9 "$job"
 
 # Run with 9.1.0
 yarn add -D ts-node@9.1.0
 yarn watch
 job="$!"
-sleep 20
+sleep "$waittime"
 kill -9 "$job"
 
 # Run with 9.1.1
 yarn add -D ts-node@9.1.1
 yarn watch
 job="$!"
-sleep 20
+sleep "$waittime"
 kill -9 "$job"
 
 echo "Process exited with code: $?"
