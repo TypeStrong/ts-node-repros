f41fe88a0a578376e97aa846fc9e4dbda6e780ec
Andrew Bradley <cspotcode@gmail.com>
2021-03-25 17:53:36 -0400

    fix

--- run2.sh
+++ run2.sh
@@ -4,50 +4,14 @@ set -x
 # Install a specific version of node
 n 12.16.1
 
-npm install -g npm-run-all gulp
+git clone https://github.com/code-asher/ts-node-repro/blob/master/index.ts
+cd ts-node-repro
 
-# install jq
-# mkdir bin
-# curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > bin/jq
-# chmod +x bin/jq
-export PATH="$PATH:$PWD/bin"
-# quick test to be sure it worked
-echo '{"foo": 1}' | jq .foo
-
-git clone https://github.com/cdr/code-server.git
-cd code-server
-# git checkout main
-git checkout jsjoeio/ts-node-repro
 yarn
+yarn ts-node index.ts
 
-waittime=60
-
-# Run with old version
-yarn watch &
-job="$!"
-sleep "$waittime"
-kill -9 "$job"
-
-# Run with 9.0.0 (explicit)
-yarn add -D ts-node@9.0.0
-yarn watch &
-job="$!"
-sleep "$waittime"
-kill -9 "$job"
-
-# Run with 9.1.0
-yarn add -D ts-node@9.1.0
-yarn watch
-job="$!"
-sleep "$waittime"
-kill -9 "$job"
-
-# Run with 9.1.1
-yarn add -D ts-node@9.1.1
-yarn watch
-job="$!"
-sleep "$waittime"
-kill -9 "$job"
+yarn add ts-node@9.0.0
+yarn ts-node index.ts
 
 echo "Process exited with code: $?"
 echo
