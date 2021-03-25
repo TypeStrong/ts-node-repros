53fd61aad0f03925f150c0b65d17058827e4eaa3
Andrew Bradley <cspotcode@gmail.com>
2021-03-25 17:02:06 -0400

    update

--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ set -x
 # Install a specific version of node
 n 12.16.1
 
+npm install -g npm-run-all
+
+# install jq
+mkdir bin
+curl https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 > bin/jq
+export PATH="$PATH:$PWD/bin"
+# quick test to be sure it worked
+echo '{"foo": 1}' | jq .foo
+
 git clone https://github.com/cdr/code-server.git
 cd code-server
 git checkout main
