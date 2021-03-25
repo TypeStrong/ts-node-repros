97a633df038a1d4febb91c544f470a38133f6a4a
Andrew Bradley <cspotcode@gmail.com>
2021-03-25 16:58:10 -0400

    initial commit

--- run.sh
+++ run.sh
@@ -1,13 +1,41 @@
 #!/usr/bin/env bash
+set -x
 
 # Install a specific version of node
-n lts
+n 12.16.1
 
-# Install package.json dependencies
+git clone https://github.com/cdr/code-server.git
+cd code-server
+git checkout main
+# git checkout jsjoeio/ts-node-repro
 yarn
 
-# Run ts-node
-yarn ts-node ./example.ts
+# Run with old version
+yarn watch &
+job="$!"
+sleep 20
+kill -9 "$job"
+
+# Run with 9.0.0 (explicit)
+yarn add -D ts-node@9.0.0
+yarn watch &
+job="$!"
+sleep 20
+kill -9 "$job"
+
+# Run with 9.1.0
+yarn add -D ts-node@9.1.0
+yarn watch
+job="$!"
+sleep 20
+kill -9 "$job"
+
+# Run with 9.1.1
+yarn add -D ts-node@9.1.1
+yarn watch
+job="$!"
+sleep 20
+kill -9 "$job"
 
 echo "Process exited with code: $?"
 echo
