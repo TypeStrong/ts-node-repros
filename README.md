ae22a3a92a0de405a0db15cf78f9c9f9b514b04e
Andrew Bradley <cspotcode@gmail.com>
2021-06-05 19:42:30 -0400

    more weird cases

---
+++ README.md
@@ -0,0 +1,2 @@
+In a monorepo situation, when importing from a module that uses `exports` declaration in package.json,
+the export declaration must have a file extension.  We need to be able to redirect this import
--- test.js
+++ test.js
@@ -11,5 +11,12 @@ if(preferTsExts) {
 
 console.log('require("foo")');
 require('foo');
+
 console.log('require("./foo")');
 require('./foo');
+
+console.log('require("./bar")');
+require('./bar');
+
+console.log('require("./baz.js")');
+require('./baz.js');
