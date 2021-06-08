c8f250315dc05302dede79f7bd69ee1f92235c9a
Andrew Bradley <cspotcode@gmail.com>
2021-06-08 15:48:46 -0400

    fix!

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ npm install
 
 # Run ts-node
 echo '
-  const a: types.EitherOr<string, number> = Math.random() > 0.5 ? "hello world" : 123;
-  const b: types.EitherOr<string, number> = {thisShouldBe: "a type error"};
+  const a: Types.EitherOr<string, number> = Math.random() > 0.5 ? "hello world" : 123;
+  const b: Types.EitherOr<string, number> = {thisShouldBe: "a type error"};
 ' | ./node_modules/.bin/ts-node ./example.ts
 
 echo "Process exited with code: $?"
