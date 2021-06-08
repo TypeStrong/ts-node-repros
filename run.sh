7aa18aba66184b9f93716d7e324e45cc6e332fa3
Andrew Bradley <cspotcode@gmail.com>
2021-06-08 15:43:09 -0400

    try again?

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# Install a specific version of node
-n lts
-
 # Install package.json dependencies
-yarn
+npm install
 
 # Run ts-node
 echo '
   const a: types.EitherOr<string, number> = Math.random() > 0.5 ? "hello world" : 123;
   const b: types.EitherOr<string, number> = {thisShouldBe: "a type error"};
-' | yarn ts-node ./example.ts
+' | ./node_modules/.bin/ts-node ./example.ts
 
 echo "Process exited with code: $?"
 echo
