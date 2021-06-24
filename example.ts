1e9ccf754b69a9962552b3c0e7754cec5adad941
dkozik <dkozik@yandex.ru>
2021-06-24 20:24:48 +0200

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,1 +1,16 @@
-console.log('Some .ts code reproducing a bug');
+const a = [
+	'Row one',
+	'Row two',
+];
+
+const b = [
+	'Row three',
+	'Row four',
+];
+
+const set = [
+	...new Set([ ...a, ...b ])
+];
+
+// If you will see empty array - this is a bug
+console.log(set);
