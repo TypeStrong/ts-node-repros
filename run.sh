dd8c11cd6d0cb7e32e72e555b742cb1a1cf3901c
Antoine Poliakov <antoinep92@gmail.com>
2021-06-25 15:07:47 +0200

    add reproducer for TypeStrong/ts-node#1380

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ n lts
 # Install package.json dependencies
 yarn
 
-# Run ts-node
-yarn ts-node ./example.ts
+# Run ts-node (multiple time to have a chance of catching the random crash)
+for n in {1..10000} ; do yarn ts-node ./example.ts || code=$? && break ; done ; sh -c "exit $code"
 
 echo "Process exited with code: $?"
 echo
