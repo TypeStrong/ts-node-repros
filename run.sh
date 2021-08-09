9c3132b6fd5348794f8f4c0a4faf73838f10133d
Andrew Bradley <cspotcode@gmail.com>
2021-08-09 19:34:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,19 +6,40 @@ set -x
 mkdir -p ~/.n
 export N_PREFIX="$PWD/.n"
 
-# Install a specific version of node
+
+function doTest() {
+  node --version
+  npm --version
+  npx --version
+  echo 'console.log(123)' | npx ts-node
+}
+
+# Test with node 12
 n 12
+doTest
 
-node --version
-npm --version
-npx --version
-echo 'console.log(123)' | npx ts-node
-
+# Test with node 14
 n lts
-node --version
-npm --version
-npx --version
-echo 'console.log(123)' | npx ts-node
+doTest
+
+# Test with node 16
+n 16
+doTest
+
+# Test with node 12, but latest npm & npx
+n 12
+npm install -g npm@latest
+doTest
+
+# Test with node 14, but latest npm & npx
+n lts
+npm install -g npm@latest
+doTest
+
+# Test with node 16, but ensure latest npm & npx (should be identical to prior node 16 test since it already ships with npm 7)
+n 16
+npm install -g npm@latest
+doTest
 
 echo "Process exited with code: $?"
 echo
