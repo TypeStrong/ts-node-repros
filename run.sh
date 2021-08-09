13caf12bcbe4aaea9f4b883baf920802b2e075a2
Andrew Bradley <cspotcode@gmail.com>
2021-08-09 19:23:51 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,18 @@
 # Install a specific version of node
 n 12
 
-# Install package.json dependencies
+set +e
+set -x
 
-# Run ts-node
+node --version
+npm --version
+npx --version
+echo 'console.log(123)' | npx ts-node
+
+n lts
+node --version
+npm --version
+npx --version
 echo 'console.log(123)' | npx ts-node
 
 echo "Process exited with code: $?"
