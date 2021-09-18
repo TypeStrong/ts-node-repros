d42ee960d1c7e8dc2c3fe2e200d3733fe73ad400
Andrew Bradley <cspotcode@gmail.com>
2021-09-18 16:16:05 -0400

    update

--- example.ts
+++ example.ts
@@ -1,1 +1,5 @@
 console.log('Hello from TypeScript in ts-node via npx' as string);
+declare const module: any;
+console.log('To prove this is running as ESM, typeof module should be undefined: ' + typeof module);
+// @ts-ignore
+console.log('To prove this is running as ESM, import.meta.url should be a URL: ' + import.meta.url);
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ node --version
 npm --version
 
 # Run ts-node
-npx ts-node ./example.ts
+npx node --loader ts-node/esm ./example.ts
 
 echo "Process exited with code: $?"
 echo
