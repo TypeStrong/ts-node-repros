a5a1566527112376417ac91e26439508adfc4385
zomars <zomars@me.com>
2022-03-09 13:05:06 -0700

    Adds fetch test repro

--- example.ts
+++ example.ts
@@ -1,1 +1,23 @@
-console.log('Some .ts code reproducing a bug');
+export async function testFunction() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      mode: "cors",
+    });
+  
+    const json = await response.json();
+    return json;
+  }
+  
+  testFunction()
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((e) => {
+      console.error(e);
+      process.exit(1);
+    });
+  
