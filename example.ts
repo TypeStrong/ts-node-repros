65ebc93ea772f69c4e82a6b3a3d30e8d43fd8ce4
Andrej Leitner <aleitner@slido.com>
2022-04-26 10:11:49 +0200

    require resolved to  newrelic.js instead of node module

--- example.ts
+++ example.ts
@@ -1,1 +1,9 @@
 console.log('Some .ts code reproducing a bug');
+
+import * as newrelic from 'newrelic';
+
+console.log('>>> newrelic: ' + JSON.stringify(newrelic));
+
+newrelic.noticeError(new Error('some error'));
+
+console.log('the end');
---
+++ newrelic.js
@@ -0,0 +1,6 @@
+'use strict'
+
+exports.config = {
+  app_name : ['ts-node-repros'],
+  license_key : 'some-license-key'
+}
