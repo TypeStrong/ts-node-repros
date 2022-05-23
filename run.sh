faecde81ac7d18ff71ff4d4efd46fd645471f082
Andrew Bradley <cspotcode@gmail.com>
2022-05-23 15:09:12 -0400

    add repro

--- run.sh
+++ run.sh
@@ -3,11 +3,9 @@
 # Install a specific version of node
 n lts
 
-# Install package.json dependencies
 yarn
 
-# Run ts-node
-yarn ts-node ./example.ts
+yarn test
 
 echo "Process exited with code: $?"
 echo
---
+++ test.ts
@@ -0,0 +1,9 @@
+import {describe, it} from 'mocha'
+import * as assert from 'assert'
+import TermSet from '@rdfjs/term-set'
+
+describe('TermSet', () => {
+    it('should be defined', () => {
+        assert.ok(TermSet)
+    })
+})
