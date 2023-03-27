1b8b7656a61aed16c53eabda287207256f58748a
Andrew Bradley <cspotcode@gmail.com>
2023-03-27 15:39:20 -0400

    update

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-# Install a specific version of node
+# Install a specific version of node.  Skip this to use whatever version you
+# already have installed.
 n lts
 
 # Install package.json dependencies
 npm install
+
+# Check version numbers
+npx ts-node -vvv
 
 # Run ts-node
 npx ts-node ./example.ts
