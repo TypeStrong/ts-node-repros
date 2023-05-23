1876ae92cd39ebf369eee927a569bf66549f9e5b
Dan Ristea <dan.ristea@protonmail.com>
2023-05-23 13:01:27 +0100

    Add repro for node 20 error not being caught

--- example.ts
+++ example.ts
@@ -1,1 +1,3 @@
-console.log('Some .ts code reproducing a bug');
+const a = (b: 1 | 2) => b + 1
+
+console.log(a(3))
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Install a specific version of node
-n lts
+n 20
 
 # Install package.json dependencies
 yarn
 
 # Run ts-node
-yarn ts-node ./example.ts
+yarn node  --experimental-loader ts-node/esm ./example.ts
 
 echo "Process exited with code: $?"
 echo
